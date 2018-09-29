c1a46debcfacf3a8e963526643d2de0ccbe17acc
KamilBest <kamil.besst@gmail.com>
2018-09-29 18:22:37 +0200

    Testing led controlling with spring boot and pi4j on raspberry pi 3

---
+++ src/main/java/jb/smarthome/LedController.java
@@ -0,0 +1,35 @@
+package jb.smarthome;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LedController {
+    final GpioController gpio = GpioFactory.getInstance();
+    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00);
+
+    @RequestMapping("/ledState")
+    @ResponseBody
+    public boolean ledState() {
+        boolean isOn = pin.isHigh();
+        System.out.println(isOn);
+        return isOn;
+    }
+
+    @RequestMapping("/turnOffLed")
+    @ResponseBody
+    public void turnOffLed() {
+        pin.setState(PinState.LOW);
+    }
+
+    @RequestMapping("/turnOnLed")
+    @ResponseBody
+    public void turnOnLed()
+    {
+        pin.setState(PinState.HIGH);
+    }
+}
